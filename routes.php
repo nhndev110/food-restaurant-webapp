1ffa493060755711dd78a4d1121798bc3dfaec6b
Nguyễn Hoàng Nhân <nguyenhoangnhannn93@gmail.com>
2023-01-06 11:24:29 +0700

    setup project

--- routes.php
+++ routes.php
@@ -1,5 +1,12 @@
 <?php
 
-require_once __DIR__.'/router.php';
+require_once __DIR__ . '/router.php';
 
-get('/', "views/index.php");
+require_once "./controllers/HomeController.php";
+
+use App\Controllers\HomeController;
+
+get('/', function () {
+  $home = new HomeController();
+  $home->index();
+});
